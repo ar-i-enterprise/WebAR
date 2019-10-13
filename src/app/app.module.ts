6d87f423d11c5fd9fc3bedb2e9bd9218ad14f7e9
ar-i-enterprise <ari2019.enterprise@gmail.com>
2019-10-13 22:45:51 +0200

    4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
-import { AframePipeModule } from "angular-aframe-pipe";
-
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AframePipeModule, FormsModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
